38d205a056f3ebe80499732e1ad347c4a9efd6d3
Tomás Soler <tomas@tjsoler.me>
2017-03-10 17:55:07 +0100

    added some base code to test the fixers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,44 +1,46 @@
 <?php
 
-$path_to_phpfmt = __DIR__.'/../../src/phpfmt.php';
+// $path_to_phpfmt = __DIR__.'/../../src/phpfmt.php';
 
-function executeCommand($args, $stdin)
-{
-    global $path_to_phpfmt;
+// function executeCommand($args, $stdin)
+// {
+//     global $path_to_phpfmt;
 
-    $arguments = '';
-    foreach ($args as $option => $value) {
-        if (is_numeric($option)) {
-            $arguments .= ' '.escapeshellarg($value);
-        } else {
-            $arguments .= " $option=".escapeshellarg($value);
-        }
-    }
+//     $arguments = '';
+//     foreach ($args as $option => $value) {
+//         if (is_numeric($option)) {
+//             $arguments .= ' '.escapeshellarg($value);
+//         } else {
+//             $arguments .= " $option=".escapeshellarg($value);
+//         }
+//     }
 
-    $cmd = sprintf('php %s %s -o=- -', $path_to_phpfmt, $arguments);
+//     $cmd = sprintf('php %s %s -o=- -', $path_to_phpfmt, $arguments);
 
-    $descriptorspec = array(
-        0 => array('pipe', 'r'), // stdin is a pipe that the child will read from
-        1 => array('pipe', 'w'), // stdout is a pipe that the child will write to
-    );
+//     $descriptorspec = array(
+//         0 => array('pipe', 'r'), // stdin is a pipe that the child will read from
+//         1 => array('pipe', 'w'), // stdout is a pipe that the child will write to
+//     );
 
-    $process = proc_open($cmd, $descriptorspec, $pipes);
+//     $process = proc_open($cmd, $descriptorspec, $pipes);
 
-    if (is_resource($process)) {
-        // $pipes now looks like this:
-        // 0 => writeable handle connected to child stdin
-        // 1 => readable handle connected to child stdout
+//     if (is_resource($process)) {
+//         // $pipes now looks like this:
+//         // 0 => writeable handle connected to child stdin
+//         // 1 => readable handle connected to child stdout
 
-        fwrite($pipes[0], $stdin); // file_get_contents('php://stdin')
-        fclose($pipes[0]);
+//         fwrite($pipes[0], $stdin); // file_get_contents('php://stdin')
+//         fclose($pipes[0]);
 
-        $stdout = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
+//         $stdout = stream_get_contents($pipes[1]);
+//         fclose($pipes[1]);
 
-        // It is important that you close any pipes before calling
-        // proc_close in order to avoid a deadlock
-        $return_value = proc_close($process);
+//         // It is important that you close any pipes before calling
+//         // proc_close in order to avoid a deadlock
+//         $return_value = proc_close($process);
 
-        return $stdout;
-    }
-}
+//         return $stdout;
+//     }
+// }
+
+require __DIR__.'/../vendor/autoload.php';
