1c4467480d5788b82714feecc2112622e6520b96
Tomás Soler <tomas@tjsoler.me>
2017-03-10 21:47:24 +0100

    removed commented code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,46 +1,11 @@
 <?php
 
-// $path_to_phpfmt = __DIR__.'/../../src/phpfmt.php';
-
-// function executeCommand($args, $stdin)
-// {
-//     global $path_to_phpfmt;
-
-//     $arguments = '';
-//     foreach ($args as $option => $value) {
-//         if (is_numeric($option)) {
-//             $arguments .= ' '.escapeshellarg($value);
-//         } else {
-//             $arguments .= " $option=".escapeshellarg($value);
-//         }
-//     }
-
-//     $cmd = sprintf('php %s %s -o=- -', $path_to_phpfmt, $arguments);
-
-//     $descriptorspec = array(
-//         0 => array('pipe', 'r'), // stdin is a pipe that the child will read from
-//         1 => array('pipe', 'w'), // stdout is a pipe that the child will write to
-//     );
-
-//     $process = proc_open($cmd, $descriptorspec, $pipes);
-
-//     if (is_resource($process)) {
-//         // $pipes now looks like this:
-//         // 0 => writeable handle connected to child stdin
-//         // 1 => readable handle connected to child stdout
-
-//         fwrite($pipes[0], $stdin); // file_get_contents('php://stdin')
-//         fclose($pipes[0]);
-
-//         $stdout = stream_get_contents($pipes[1]);
-//         fclose($pipes[1]);
-
-//         // It is important that you close any pipes before calling
-//         // proc_close in order to avoid a deadlock
-//         $return_value = proc_close($process);
-
-//         return $stdout;
-//     }
-// }
-
 require __DIR__.'/../vendor/autoload.php';
+
+// we create a new instance of BaseCodeFormatter
+// only because I don't want to instantiate or 
+// copy / paste all the constants we need.
+$removeFromHerePlease =new Fmt\BaseCodeFormatter;
+
+// we probably need to find a better way to
+// accomplish this.
