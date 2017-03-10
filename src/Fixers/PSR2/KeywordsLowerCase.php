02389e03f697b35b336cd88da7bd840ab7d6bdbd
Tomás Soler <tomas@tjsoler.me>
2017-03-10 22:37:57 +0100

    psr2 keywords to lowercase test

--- src/Fixers/PSR2/KeywordsLowerCase.php
+++ src/Fixers/PSR2/KeywordsLowerCase.php
@@ -77,11 +77,14 @@ class KeywordsLowerCase extends FormatterPass implements FixerInterface
         'var',
         'while',
         'xor',
+        'true',
+        'false',
+        'null'
     ];
    
     public function candidate($source, $foundTokens)
     {
-        return true;
+        return $value = str_contains($source, static::$reservedWords);
     }
 
     public function format($source)
@@ -132,7 +135,7 @@ class KeywordsLowerCase extends FormatterPass implements FixerInterface
                         T_NS_SEPARATOR, T_AS, T_CLASS, T_EXTENDS, T_IMPLEMENTS, T_INSTANCEOF, T_INTERFACE, T_NEW, T_NS_SEPARATOR, T_PAAMAYIM_NEKUDOTAYIM, T_USE, T_TRAIT, T_INSTEADOF, T_CONST,
                     ])
                 ) ||
-                isset(static::$reservedWords[$lcText])
+                in_array($lcText, static::$reservedWords)
             ) {
                 $text = $lcText;
             }
---
+++ tests/Unit/Fixers/PSR2/KeywordsLowerCase/wrong.php
@@ -0,0 +1,35 @@
+<?php
+
+NAMESPACE Qualified\Name;
+
+ABSTRACT CLASS SomethingHere {
+    PUBLIC $something = TRUE;
+    PROTECTED $isHere = FALSE;
+    PRIVATE $nothing = NULL;
+    PRIVATE $testArray = ARRAY('an', 'array', 'here');
+
+    public FUNCTION __construct()
+    {
+        CONST Something = 'this is a const';
+
+        SWITCH ($this->testArray) {
+            CASE 'an':
+                // do nothing, because this is only a test class...
+                BREAK;
+            
+            DEFAULT:
+                // code...
+                break;
+
+            IF($this->testArray[1] == 'array') {
+                DIE();
+            }
+            ELSEIF($this->testArray[1] != 'array') {
+                ECHO 'something';
+            }
+            ELSE {
+                $this->something = CLONE $this->testArray;
+            }
+        }
+    }
+}
