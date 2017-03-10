8ffdb586b7cde2e222ec2fe6077be46c9b8d7bfb
Tomás Soler <tomas@tjsoler.me>
2017-03-10 21:49:40 +0100

    fail

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__.'/../vendor/autoload.php';
 // we create a new instance of BaseCodeFormatter
 // only because I don't want to instantiate or 
 // copy / paste all the constants we need.
-$removeFromHerePlease =new Fmt\BaseCodeFormatter;
+$removeFromHerePlease =new Fmt\CodeFormatter;
 
 // we probably need to find a better way to
 // accomplish this.
