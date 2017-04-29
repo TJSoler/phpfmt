9195440bd738768c5d3e7cdaf782611b416dbae5
Tomás Soler <tomas@tjsoler.me>
2017-04-29 17:16:51 +0200

    cleanup

--- src/Fixers/ReindentSwitchBlocks.php
+++ src/Fixers/ReindentSwitchBlocks.php
@@ -17,13 +17,13 @@ class ReindentSwitchBlocks extends FormatterPass implements FixerInterface
         if (isset($foundTokens[T_SWITCH])) {
             return true;
         }
+
         return false;
     }
 
     public function format($source)
     {
         $this->tkns = token_get_all($source);
-        
         $this->code = '';
         $touchedSwitch = false;
         $foundStack = [];
@@ -31,8 +31,9 @@ class ReindentSwitchBlocks extends FormatterPass implements FixerInterface
         while (list($index, $token) = each($this->tkns)) {
             list($id, $text) = $this->getToken($token);
             $this->ptr = $index;
+
             switch ($id) {
-                case ":":
+                case ST_COLON:
                     $this->appendCode($text);
                     $this->setIndent(+1);
                     break;
@@ -46,21 +47,26 @@ class ReindentSwitchBlocks extends FormatterPass implements FixerInterface
                     $this->appendCode($text);
                     $this->printUntilTheEndOfString();
                     break;
+
                 case T_CLOSE_TAG:
                     $this->appendCode($text);
                     $this->printUntil(T_OPEN_TAG);
                     break;
+
                 case T_START_HEREDOC:
                     $this->appendCode($text);
                     $this->printUntil(T_END_HEREDOC);
                     break;
+
                 case T_CONSTANT_ENCAPSED_STRING:
                     $this->appendCode($text);
                     break;
+
                 case T_SWITCH:
                     $touchedSwitch = true;
                     $this->appendCode($text);
                     break;
+
                 case T_DOLLAR_OPEN_CURLY_BRACES:
                 case T_CURLY_OPEN:
                 case ST_CURLY_OPEN:
@@ -73,6 +79,7 @@ class ReindentSwitchBlocks extends FormatterPass implements FixerInterface
                     }
                     $foundStack[] = $indentToken;
                     break;
+
                 case ST_CURLY_CLOSE:
                     $poppedID = array_pop($foundStack);
                     if (T_SWITCH === $poppedID) {
@@ -80,21 +87,30 @@ class ReindentSwitchBlocks extends FormatterPass implements FixerInterface
                     }
                     $this->appendCode($text);
                     break;
+
                 default:
                     $hasLn = $this->hasLn($text);
                     if ($hasLn) {
                         $poppedID = end($foundStack);
-                        if (
-                            T_SWITCH == $poppedID &&
+                        if (T_SWITCH == $poppedID &&
                             $this->rightTokenIs(ST_CURLY_CLOSE)
-                       ) {
+                        ) {
                             $this->setIndent(-1);
-                            $text = str_replace($this->newLine, $this->newLine . $this->getIndent(), $text);
+                            $text = str_replace(
+                                $this->newLine,
+                                $this->newLine . $this->getIndent(),
+                                $text
+                            );
                             $this->setIndent(+1);
                         } else {
-                            $text = str_replace($this->newLine, $this->newLine . $this->getIndent(), $text);
+                            $text = str_replace(
+                                $this->newLine,
+                                $this->newLine . $this->getIndent(),
+                                $text
+                            );
                         }
                     }
+                    
                     $this->appendCode($text);
                     break;
             }
